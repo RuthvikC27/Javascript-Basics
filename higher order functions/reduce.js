65dce23c784545e7b9599a5e13b6ba7fa21cc46c
Ruthvik Vanam <ruthvikvanam27@gmail.com>
2020-08-19 23:33:08 +0530

    added promises

--- higher order functions/reduce.js
+++ higher order functions/reduce.js
@@ -1,4 +1,7 @@
 // var vals = Array(5).fill(0).map(() => Math.random());
 var vals = [1, 2, 3, 4, 5]
-vals = vals.reduce((acc, val) => acc + val, 10)
+vals = vals.reduce((acc, val) => {
+    console.log(acc)
+    return acc + val
+}, 10)
 console.log(vals)
